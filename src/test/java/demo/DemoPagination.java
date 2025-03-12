fa5e561d87db2a42503cba98149cdd5393a66e77
Gokul9812 <gokulrit25@gmail.com>
2025-03-12 10:37:37 +0530

    Second commit

--- src/test/java/demo/DemoPagination.java
+++ src/test/java/demo/DemoPagination.java
@@ -35,6 +35,17 @@ public class DemoPagination {
         // Additional test steps can go here...
     }
 
+    
+    @Test
+    public void Pagination1() {
+        // Set implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Open the demo site
+        driver.get("https://demo3x.opencartreports.com/admin/");
+        
+        // Additional test steps can go here...
+    }
     @After
     public void tearDown() {
         // Close the browser after the test completes
